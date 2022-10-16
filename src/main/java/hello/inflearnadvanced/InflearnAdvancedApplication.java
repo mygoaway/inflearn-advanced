f8eb4bb30bbfca445d111b327537a37dbc117341
mygoaway <mygoaway2@gmail.com>
2022-10-16 20:27:10 +0900

    initial commit

---
+++ src/main/java/hello/inflearnadvanced/InflearnAdvancedApplication.java
@@ -0,0 +1,13 @@
+package hello.inflearnadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnAdvancedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/inflearnadvanced/InflearnAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.inflearnadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnAdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
